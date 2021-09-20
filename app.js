0098ba3395dd2391e9d6f7ccf3e0fedbbd68beb2
Samson Jisso <chunaj300@gmail.com>
2021-09-20 11:01:46 +0300

    commit

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require("express");
+const filterMidWare = require("./filterMidWare");
+const countMiddleWare = require("./countMiddleWare");
+const controller = require("./controller");
+const sortMidWare = require("./sortMidWare");
 const app = express();
+app.use(express.json());
+//app.use([filter,reg,counter,sorter])
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json);
+
+app.post("/emoji", filterMidWare, countMiddleWare, sortMidWare, controller);
+// function one  :  validation -> validates the string and checks if any unwanted charcters exist
+// function two : sorts and compares -> hint (uses map for storing the characters and their respective count)
+// if the hash map contains the character increament by one
+// if the hash map doesnt contain the character set the character and set the value to one
+// function three :replaces the top four to emoji ->
 app.get("/", (req, res) => {
-  console.log(req.body);
+  let data = req.body;
+  console.log(data);
+  res.send();
 });
-app.listen(5000, () => {
-  console.log("Server is listening on port 5000...");
+app.listen(6000, () => {
+  console.log("server is listening on port 6000....");
 });
