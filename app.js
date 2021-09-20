83870c1dcd982c8b196bc17d2be8be8ed3bab76b
Samson Jisso <chunaj300@gmail.com>
2021-09-20 10:24:17 +0300

    test

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json);
 app.get("/", (req, res) => {
-  let data = req.body;
-  console.log(data);
-  res.send(req.body);
+  console.log(req.body);
 });
 app.listen(5000, () => {
   console.log("Server is listening on port 5000...");
