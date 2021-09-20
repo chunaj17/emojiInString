00873359b67b0c6cb053d50e2801cfe7c9ee9f31
Samson Jisso <chunaj300@gmail.com>
2021-09-20 10:10:52 +0300

    new

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
 const express = require("express");
-const promise = require("./promiseMidWare");
-const { writeFileSync, readFile } = require("fs");
-const filterMidWare = require("./filterMidWare");
-const countMiddleWare = require("./countMiddleWare");
-const controller = require("./controller");
-const sortMidWare = require("./sortMidWare");
 const app = express();
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-//app.use([filter,reg,counter,sorter])
-app.use(express.urlencoded({ extended: false }));
-app.get("/promise", filterMidWare, promise);
-app.get("/json", filterMidWare, (req, res) => {
-  let j = new Promise((resolve, reject) => {
-    readFile("./data.json", "utf8", (req, res) => {});
-
-    // Trail one code d
-  });
+app.use(express.json);
+app.get("/", (req, res) => {
+  let data = req.body;
+  console.log(data);
+  res.send(req.body);
 });
-app.post("/emoji", filterMidWare, countMiddleWare, sortMidWare, controller);
-// function one  :  validation -> validates the string and checks if any unwanted charcters exist
-// function two : sorts and compares -> hint (uses map for storing the characters and their respective count)
-// if the hash map contains the character increament by one
-// if the hash map doesnt contain the character set the character and set the value to one
-// function three :replaces the top four to emoji ->
-app.listen(6000, () => {
-  console.log("server is listening on port 6000....");
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000...");
 });
