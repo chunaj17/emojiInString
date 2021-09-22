01da28569e0aea63dce1fab7c0e9a239bdc2ff2b
Samson Jisso <chunaj300@gmail.com>
2021-09-22 04:43:20 +0300

    commit

--- jsonController.js
+++ jsonController.js
@@ -1,17 +1,29 @@
 const fs = require("fs");
+const emoji = require("node-emoji");
+const jsonEmoji = require("./emoji.json");
+// const emojiJson = JSON.parse(jsonEmoji);
 let jsonController = (req, res) => {
-  let userJson = req.body;
-  let id = userJson.id;
-  let characters = userJson.characters;
+  let { id, characters } = req.body;
   fs.readFile("./customer.json", "utf8", (err, jsonObj) => {
     if (err) {
       res.send("error occured while reading json", err);
     } else {
       let data = JSON.parse(jsonObj);
+      let parsed = "";
       let time = new Date();
-      console.log(time.getSeconds());
-      let parsed = characters.replace(characters.replace(/\s+/g).split("%"));
+      //   let parsed = characters.replace(/\s+/g).split("%");
+
+      for (let i = 0, n = characters.length; i < n; i++) {
+        // console.log(i);
+        let c = characters.charAt(i).toLowerCase();
+        // console.log(c);
+        // parsed = characters.replace(c, emoji.get(c));
+        if (emoji.get(c)) {
+          parsed += emoji.get("smile");
+        }
+      }
       console.log(parsed);
+
       if (data.hasOwnProperty(id)) {
         return res.send(" The data is already available");
       } else {
